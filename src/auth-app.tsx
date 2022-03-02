1e5ab818ef683b8fb2584c8cc1c39e83bb7b63a5
赵小强 <944974339@qq.com>
2022-03-02 23:52:16 +0800

    用Row布局页面

--- src/auth-app.tsx
+++ src/auth-app.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen></ProjectListScreen>
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen></ProjectListScreen>
+      </Main>
+    </Container>
   );
 };
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.div``;
